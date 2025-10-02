f2c0b39239553b7d75a3bb21f73cbd72199764fd
vvvvvv eeeeee <hiddenether@yahoo.com>
2025-10-01 23:36:56 -0400

    Added baasic CSS to make the Shop look better

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <>
+    <div>
       <Link to='/'><strong>Shop</strong></Link>
       <Link style={{marginLeft: "1rem"}} to='/cart'><strong>Cart</strong></Link>
-    </>
+    </div>
   );
 }

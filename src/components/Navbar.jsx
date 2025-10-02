ff64b6b8e38155bf59cd10140b06ae70ad8d79bb
vvvvvv eeeeee <hiddenether@yahoo.com>
2025-10-01 23:37:47 -0400

    further CSS changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div>
+    <div className='navbar'>
       <Link to='/'><strong>Shop</strong></Link>
       <Link style={{marginLeft: "1rem"}} to='/cart'><strong>Cart</strong></Link>
     </div>
